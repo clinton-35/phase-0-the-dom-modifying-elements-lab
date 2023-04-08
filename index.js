13bdfa5e102e141f6b0742e8f4b733eba882cb02
clinton <clinton.wambua@student.moringaschool.com>
2023-04-08 07:20:37 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// get a reference to the main element
+const main = document.querySelector('main');
+
+// remove the main element from the DOM
+main.remove();
+
+// create a new header element
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// append the new header element to the body element
+document.querySelector('body').appendChild(newHeader);
+
+
